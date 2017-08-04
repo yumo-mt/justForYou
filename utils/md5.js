04432f75344c033ed73ab28a184053e2daac1208
Manster <mansterhaihai@sina.com>
2017-08-04 16:23:01 +0800

    build

--- utils/md5.js
+++ utils/md5.js
@@ -1,6 +1,6 @@
 module.exports = {
-    md5:function (str) {
-        //先加载crypto模块，创建md5哈希算法，指定要加密的字符串，再进行摘要按16进制输出 sha1
-        return require('crypto').createHash('md5').update(str).digest('hex')
-    }
+  md5: function (str) {
+    //先加载crypto模块，创建md5哈希算法，指定要加密的字符串，再进行摘要按16进制输出 sha1
+    return require('crypto').createHash('md5').update(str).digest('hex')
+  }
 }
