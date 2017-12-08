3e56a6f62a943637d46b86ce1e97b609ea1dbaf8
Manster <rongchanghai@users.noreply.github.com>
2017-12-08 15:40:58 +0800

    Update settings.js

--- justForYou/settings.js
+++ justForYou/settings.js
@@ -2,8 +2,6 @@ module.exports = {
   cookieSecret: 'justForYou',
   //div-data
   //设置用户名和密码
-  url:'mongodb://manster:5092952@localhost:27017/justForYou'
-  //online-data
-  // url: 'mongodb://manster:5092952@114.215.80.72:27017/justForYou'
-  // url: 'mongodb://114.215.80.72:27017/justForYou'
+  //url:'mongodb://username:password@localhost:27017/justForYou'
+   url: 'mongodb://domainUrl:27017/justForYou'
 }
